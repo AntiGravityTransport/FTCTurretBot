921736dbb011430dee588a1b251cbd42d3dbaf77
I.D. <146482178+AntiGravityTransport@users.noreply.github.com>
2026-02-05 18:52:41 -0600

    Update Odometry.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 public class Odometry {
@@ -14,10 +12,13 @@ public class Odometry {
 
         /*
          * SETTINGS: Adjust these based on your physical robot build.
-         * The offsets are the distance from the center of the robot to the pods.
+         * The offsets are the distance from the center of the robot to the pods (in mm).
          */
-        pinpoint.setOffsets(-84.0, -168.0); // Example offsets in mm
-        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods. those_4_bar_pods);
+        pinpoint.setOffsets(-84.0, -168.0);
+
+        // Revised resolution setting to match the standard driver's naming convention
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
         pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
         pinpoint.resetPosAndIMU();
@@ -29,9 +30,5 @@ public class Odometry {
 
     public Pose2D getPose() {
         return pinpoint.getPosition();
-    }
-    
-    public String getStatus() {
-        return pinpoint.getDeviceStatus().toString();
     }
 }
